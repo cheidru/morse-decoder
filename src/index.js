7e388973684887fc682990133bd5793be23c78d6
My Name <my@test.com>
2021-08-23 01:40:22 +0300

    fix: fix errors

--- src/index.js
+++ src/index.js
@@ -46,9 +46,9 @@ function decode(expr) {
     let morseSubstr = "";
     for (let i = 0; i < exprString.length; i += 10) {
         // result = result + MORSE_TABLE[expr.substr(i, i+10)];
-        morseSubstr = exprString.substr(i, i+10);
-        // result = result + `${morseSubstr == '**********' ? ' ' : MORSE_TABLE[turnToMorseCode(morseSubstr)]}`;
-        result = result + ' ' + morseSubstr + ' ' + turnToMorseCode(morseSubstr);
+        morseSubstr = exprString.substr(i, 10);
+        result = result + morseSubstr == '**********' ? ' ' : MORSE_TABLE[turnToMorseCode(morseSubstr)];
+        // result = result + ' ' + morseSubstr + ' ' + turnToMorseCode(morseSubstr);
     }
 
     return result;
