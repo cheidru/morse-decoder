67f8f750dc0a6f6705cd52ce9b196ef5c727cff3
My Name <my@test.com>
2021-08-22 22:50:36 +0300

    init: create js code

--- src/index.js
+++ src/index.js
@@ -39,6 +39,12 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    MORSE_TABLE['**********'] = ' ';
+    let result = "";
+    for (i = 0; i < expr.length; i+=10) {
+        result += MORSE_TABLE[expr.substr(i, i+10)]
+    }
+    return result;
 }
 
 module.exports = {
