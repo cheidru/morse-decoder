16fb93449b2d03d7e9f4dd6d7ddce5d4fd9f707d
My Name <my@test.com>
2021-08-23 01:06:08 +0300

    fix: fix errors

--- src/index.js
+++ src/index.js
@@ -47,35 +47,23 @@ function decode(expr) {
     for (let i = 0; i < exprString.length; i += 10) {
         // result = result + MORSE_TABLE[expr.substr(i, i+10)];
         morseSubstr = exprString.substr(i, i+10);
-        if (morseSubstr == '**********') {
-            result = result + ' ';
-        } else {
-            let digs = morseSubstr.split('');
-            let j = 0;
-            let morse = "";
-            while (digs[j] == 0) j++;
-            for (; j < morseSubstr.length; j += 2){
-                morse = digs[j] + digs[j+1] == '10' ? morse + '.' : morse + '-';
-            }
-            result = result + MORSE_TABLE[morse];
-
-        }
         // result = result + `${morseSubstr == '**********' ? ' ' : MORSE_TABLE[turnToMorseCode(morseSubstr)]}`;
+        result = result + turnToMorseCode(morseSubstr);
     }
 
     return result;
 }
 
-// function turnToMorseCode(str) {
-//     let digs = str.split('');
-//     let i = 0;
-//     let morse = "";
-//     while (digs[i] == 0) i++;
-//     for (; i < str.length; i += 2){
-//         morse = digs[i] + digs[i+1] == '10' ? morse + '.' : morse + '-';
-//     }
-//     return morse;
-// }
+function turnToMorseCode(str) {
+    let digs = str.split('');
+    let i = 0;
+    let morse = "";
+    while (digs[i] == 0) i++;
+    for (; i < str.length; i += 2){
+        morse = digs[i] + digs[i+1] == '10' ? morse + '.' : morse + '-';
+    }
+    return morse;
+}
 
 module.exports = {
     decode
